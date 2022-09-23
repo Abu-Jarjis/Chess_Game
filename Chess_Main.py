27c42f202e65bff1d37592198aaf56d3c1827f7e
Abu Jarjis <80599115+Abu-Jarjis@users.noreply.github.com>
2022-09-23 04:34:53 -0700

    Update Chess_Main.py

--- Chess_Main.py
+++ Chess_Main.py
@@ -13,13 +13,13 @@ np.set_printoptions(threshold=sys.maxsize)
 res_x = res_y = 512
 screen = pg.display.set_mode((res_x,res_y))
 
-
 #board
 board = Board(res_x)
 board.preload_images()
 
+
 #for i in board.COORDINATES.keys():
-    #print(i)
+
 
 
 
@@ -27,13 +27,32 @@ board.preload_images()
 piece = Piece(res_x)
 piece_classes = {"B":Bishop(res_x), "N":Knight(res_x), "R":Rook(res_x), "K":King(res_x), "Q": Queen(res_x), "p":Pawn(res_x)}
 
-def move_handler(piece_classes, Piece, Board, event, screen):
-        if (event.button, Board.game_state) == (1,1):
+'''def move_handler(piece_classes, Piece, Board, event, screen):
+        if event.button == 1:
+            new_pos = Piece.sqr_select(pg.mouse.get_pos())
+
+            if Piece.prev_pos == new_pos:
+                Board.game_state = 1
+                print(Piece.piece_type_color(new_pos, Board)[0][0] , Piece.cur_turn)
+                return
+            
+            if Piece.piece_type_color(new_pos, Board)[0][0] == Piece.cur_turn:
+                Board.game_state = 1
+                print("VALID")
+                return
+
+            if new_pos in Piece.allowed:
+                Piece.mov_piece(new_pos, Board)
+                Board.move_log.append((Board.cur_turn, Piece.piece_type_color(new_pos, Board)[0][1], new_pos))
+            else:
+                Board.error(new_pos, screen)
+                
+            print(new_pos)
+            Board.game_state = 1
+def check_allowed(piece_classes, Piece, Board, event, screen):
+    if (event.button, Board.game_state) == (1,1):
                 cur_pos = Piece.sqr_select(pg.mouse.get_pos())
-                print(cur_pos)
-                if Piece.check_if_turn(cur_pos, screen, Board):
-                    pass
-                else:
+                if not Piece.check_if_turn(cur_pos, screen, Board):
                     return
 
                 if cur_pos in Board.COORDINATES.keys():
@@ -46,18 +65,46 @@ def move_handler(piece_classes, Piece, Board, event, screen):
                 else:
                     Board.error(cur_pos, screen)
                     #pg.display.update()
-                    time.sleep(0.2)
+                    time.sleep(0.5)
+'''
+
+def move_handler(piece_classes, Piece, Board, event, screen):
+        if (event.button, Board.game_state) == (1,1):
+                cur_pos = Piece.sqr_select(pg.mouse.get_pos())
+                if not Piece.check_if_turn(cur_pos, screen, Board):
+                    return
+
+                if cur_pos in Board.COORDINATES.keys():
+                    Piece.current_piece = Board.COORDINATES[cur_pos]
+                    Board.game_state = 0
+                    Piece.prev_pos = cur_pos
+                    prev_piece = Piece.piece_type_color(Piece.prev_pos, Board)
+
+                    Piece.allowed = piece_classes[prev_piece[0][1]].allowed_moves(cur_pos, prev_piece[1], Board)
+                else:
+                    Board.error(cur_pos, screen)
+                    #pg.display.update()
+                    time.sleep(0.5)
+
         elif event.button == 1:
             new_pos = Piece.sqr_select(pg.mouse.get_pos())
 
             if Piece.prev_pos == new_pos:
                 Board.game_state = 1
                 return
+
+            if Piece.piece_type_color(new_pos, Board)[0][0] == Piece.cur_turn:
+                Board.game_state = 1
+                print("working")
+                return True
                 
             if new_pos in Piece.allowed:
                 Piece.mov_piece(new_pos, Board)
+                Board.move_log.append((Board.cur_turn, Piece.piece_type_color(new_pos, Board)[0][1], new_pos))
+            
             else:
                 Board.error(new_pos, screen)
+            print(new_pos)
             Board.game_state = 1
     
 
@@ -70,12 +117,13 @@ while True:
             pg.quit()
             sys.exit()
         if event.type == pg.MOUSEBUTTONDOWN:
-            move_handler(piece_classes, piece, board, event, screen)
+            if move_handler(piece_classes, piece, board, event, screen):
+                move_handler(piece_classes, piece, board, event, screen)
     if not board.game_state:
-        piece
-    (piece.allowed, screen)
+        piece.highlight_valid(piece.allowed, screen, int(res_x // 16))
+    #print(board.move_log)
 
     board.load_pieces(screen)    
     pg.display.update()
-    clock.tick(15)
+    clock.tick(30)
     
