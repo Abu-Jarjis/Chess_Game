2d3b7cdb2ea5ddb2e724795db237868433f4bcd5
Abu Jarjis <80599115+Abu-Jarjis@users.noreply.github.com>
2023-03-30 07:25:27 -0700

    Update Chess_Main.py

--- Chess_Main.py
+++ Chess_Main.py
@@ -2,128 +2,172 @@ import pygame as pg
 import numpy as np
 import sys
 import time
-from Chess_Engine import Board, Piece
 from PIeces import *
+from Chess_Engine import Board
 
 pg.init()
 clock = pg.time.Clock()
 np.set_printoptions(threshold=sys.maxsize)
 
-#screen
+#SCREEN
 res_x = res_y = 512
-screen = pg.display.set_mode((res_x,res_y))
+SCREEN = pg.display.set_mode((res_x,res_y), pg.RESIZABLE)
 
-#board
-board = Board(res_x)
-board.preload_images()
-
-
-#for i in board.COORDINATES.keys():
-
-
-
+#BOARD
+BOARD = Board(res_x)
+BOARD.preload_images()
 
 #pieces
-piece = Piece(res_x)
-piece_classes = {"B":Bishop(res_x), "N":Knight(res_x), "R":Rook(res_x), "K":King(res_x), "Q": Queen(res_x), "p":Pawn(res_x)}
+PIECE = Piece(res_x)
 
-'''def move_handler(piece_classes, Piece, Board, event, screen):
-        if event.button == 1:
-            new_pos = Piece.sqr_select(pg.mouse.get_pos())
 
-            if Piece.prev_pos == new_pos:
-                Board.game_state = 1
-                print(Piece.piece_type_color(new_pos, Board)[0][0] , Piece.cur_turn)
-                return
-            
-            if Piece.piece_type_color(new_pos, Board)[0][0] == Piece.cur_turn:
-                Board.game_state = 1
-                print("VALID")
-                return
-
-            if new_pos in Piece.allowed:
-                Piece.mov_piece(new_pos, Board)
-                Board.move_log.append((Board.cur_turn, Piece.piece_type_color(new_pos, Board)[0][1], new_pos))
+def if_check(king_allowed_movs, king_square, cur_pos, enemy_attack_squares):
+    if "King_Checked" in king_allowed_movs:
+            print("CHECKED_KING")
+            if cur_pos == king_square:
+                print("SELECT_KING")
+                cur_pos = king_square
+                #PIECE.current_piece = BOARD.COORDINATES[cur_pos]
+                PIECE.prev_pos = cur_pos
+                prev_piece = PIECE.piece_type_color(PIECE.prev_pos, BOARD)
+                PIECE.allowed = PIECE_CLASSES[prev_piece[0][1]].allowed_moves(cur_pos, prev_piece[1], BOARD, PIECE, sqr_attacks = enemy_attack_squares[0][::])[1]
             else:
-                Board.error(new_pos, screen)
-                
-            print(new_pos)
-            Board.game_state = 1
-def check_allowed(piece_classes, Piece, Board, event, screen):
-    if (event.button, Board.game_state) == (1,1):
-                cur_pos = Piece.sqr_select(pg.mouse.get_pos())
-                if not Piece.check_if_turn(cur_pos, screen, Board):
+                print("ERROR")
+                BOARD.error(cur_pos, SCREEN)
+                return False
+    return True
+
+def still_check(board):
+    
+
+    pass
+    
+
+def castle_rook(new_pos):
+    castle_left =  PIECE_CLASSES["K"].queen_side_castle_movs[1]
+    castle_right = PIECE_CLASSES["K"].king_side_castle_movs[-1]
+
+    if new_pos[0] == castle_right:
+        new_castle_pos = new_pos[0] - PIECE.sqr_size
+        PIECE.prev_pos = (castle_right + PIECE.sqr_size, new_pos[1])
+        PIECE.mov_piece(( new_castle_pos, new_pos[1]), BOARD)
+
+    elif new_pos[0] == castle_left:
+        new_castle_pos = new_pos[0] + PIECE.sqr_size
+        PIECE.prev_pos = (castle_left - 2*PIECE.sqr_size, new_pos
+        [1])
+        PIECE.mov_piece((new_castle_pos, new_pos[1]), BOARD)
+    pass
+
+
+def move_handler(event):
+        if (event.button, BOARD.game_state) == (1,3):
+            BOARD.game_state = 0
+            return
+            pass
+        if (event.button, BOARD.game_state) == (1,1):
+                cur_pos = PIECE.sqr_select(pg.mouse.get_pos())
+                if not PIECE.check_if_turn(cur_pos, SCREEN, BOARD):
                     return
 
-                if cur_pos in Board.COORDINATES.keys():
-                    Piece.current_piece = Board.COORDINATES[cur_pos]
-                    Board.game_state = 0
-                    Piece.prev_pos = cur_pos
-                    prev_piece = Piece.piece_type_color(Piece.prev_pos, Board)
+                if cur_pos in BOARD.COORDINATES.keys():
+                    #§§§PIECE.current_piece = BOARD.COORDINATES[cur_pos]
+                    
+                    PIECE.prev_pos = cur_pos
+                    prev_piece = PIECE.piece_type_color(PIECE.prev_pos, BOARD)
+                    enemy_attack_squares = PIECE.check_enemy_attacks(BOARD, PIECE)
+                    king_square, king_color = enemy_attack_squares[1], enemy_attack_squares[2]
+                    king_allowed_movs = PIECE_CLASSES["K"].allowed_moves(king_square, king_color, BOARD, PIECE, is_attack = False, sqr_attacks = enemy_attack_squares[0][::])
+                    
+                    if prev_piece[0][1] == "K":
+                        PIECE.allowed = king_allowed_movs
+                    else:
+                        PIECE.allowed = PIECE_CLASSES[prev_piece[0][1]].allowed_moves(cur_pos, prev_piece[1], BOARD, PIECE)
+                    
+                    #Checks if king is in check
+                    #and runs function
+                    #
+                    #
+                    if not if_check(king_allowed_movs, king_square, cur_pos, enemy_attack_squares):
+                        return
+                            
+                        
 
-                    Piece.allowed = piece_classes[prev_piece[0][1]].allowed_moves(cur_pos, prev_piece[1], Board)
+                    BOARD.game_state = 0
+
+
+
+
                 else:
-                    Board.error(cur_pos, screen)
-                    #pg.display.update()
-                    time.sleep(0.5)
-'''
-
-def move_handler(piece_classes, Piece, Board, event, screen):
-        if (event.button, Board.game_state) == (1,1):
-                cur_pos = Piece.sqr_select(pg.mouse.get_pos())
-                if not Piece.check_if_turn(cur_pos, screen, Board):
-                    return
-
-                if cur_pos in Board.COORDINATES.keys():
-                    Piece.current_piece = Board.COORDINATES[cur_pos]
-                    Board.game_state = 0
-                    Piece.prev_pos = cur_pos
-                    prev_piece = Piece.piece_type_color(Piece.prev_pos, Board)
-
-                    Piece.allowed = piece_classes[prev_piece[0][1]].allowed_moves(cur_pos, prev_piece[1], Board)
-                else:
-                    Board.error(cur_pos, screen)
+                    BOARD.error(cur_pos, SCREEN)
                     #pg.display.update()
                     time.sleep(0.5)
 
         elif event.button == 1:
-            new_pos = Piece.sqr_select(pg.mouse.get_pos())
+            new_pos = PIECE.sqr_select(pg.mouse.get_pos())
 
-            if Piece.prev_pos == new_pos:
-                Board.game_state = 1
+
+            if PIECE.prev_pos == new_pos:
+                BOARD.game_state = 1
                 return
 
-            if Piece.piece_type_color(new_pos, Board)[0][0] == Piece.cur_turn:
-                Board.game_state = 1
-                print("working")
+            if PIECE.piece_type_color(new_pos, BOARD)[0][0] == PIECE.cur_turn:
+                BOARD.game_state = 1
                 return True
                 
-            if new_pos in Piece.allowed:
-                Piece.mov_piece(new_pos, Board)
-                Board.move_log.append((Board.cur_turn, Piece.piece_type_color(new_pos, Board)[0][1], new_pos))
+            if new_pos in PIECE.allowed:
+                cur_piece = PIECE.piece_type_color(PIECE.prev_pos, BOARD)[0]
+                PIECE.mov_piece(new_pos, BOARD)
+                prev_sqr, after_sqr = PIECE.move_logging(new_pos, BOARD)
+
+                if PIECE.cur_turn == "w":
+                    PIECE.W_moves.append((cur_piece, prev_sqr, after_sqr))
+                if PIECE.cur_turn == "b":
+                    PIECE.B_moves.append((cur_piece, prev_sqr, after_sqr))
+                
+                
             
+                print(cur_piece)
+                if cur_piece[1] == "K":
+                    print("WORKS")
+                    if PIECE_CLASSES["K"].W_King_startsqr == PIECE.prev_pos or PIECE_CLASSES["K"].B_King_startsqr == PIECE.prev_pos:
+                        print("WORKS")
+                        castle_rook(new_pos)
+                    
+                PIECE.cur_turn = "b" if PIECE.cur_turn == "w" else "w"
+                    
+
             else:
-                Board.error(new_pos, screen)
-            print(new_pos)
-            Board.game_state = 1
+                BOARD.error(new_pos, SCREEN)
+                
+            BOARD.game_state = 1
+
+
+def Run_Chess():
     
 
-while True:
-    screen.fill((0,0,0))
-    board.draw_board(screen)
+    while True:
+        SCREEN.fill((0,0,0))
+        BOARD.draw_board(SCREEN)
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
-        if event.type == pg.MOUSEBUTTONDOWN:
-            if move_handler(piece_classes, piece, board, event, screen):
-                move_handler(piece_classes, piece, board, event, screen)
-    if not board.game_state:
-        piece.highlight_valid(piece.allowed, screen, int(res_x // 16))
-    #print(board.move_log)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if move_handler(event):
+                    move_handler(event)
+            if event.type == pg.VIDEORESIZE:
+                pass
+        #PIECE.highlight_valid(PIECE.check_enemy_attacks(BOARD,PIECE)[0], SCREEN, int(res_x // 16))
+        if not BOARD.game_state:
+            PIECE.highlight_valid(PIECE.allowed, SCREEN, int(res_x // 16))
+        #(BOARD.move_log)
+        #PIECE.highlight_valid(PIECE.check_enemy_attacks(BOARD, PIECE)[0], SCREEN, int(res_x // 16))
+        #PIECE.highlight_valid([[128,448]], SCREEN, int(res_x//16))
+        BOARD.load_pieces(SCREEN)    
+        pg.display.update()
+        clock.tick(30)
 
-    board.load_pieces(screen)    
-    pg.display.update()
-    clock.tick(30)
+Run_Chess()
     
